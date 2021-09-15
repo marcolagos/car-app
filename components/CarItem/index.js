86322aa8e92f3807f562e1933884e2f485d02d4b
marcolagos <84419908+marcolagos@users.noreply.github.com>
2021-09-14 20:43:42 -0500

    CarItem finished

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -5,16 +5,26 @@ import styles from './styles';
 
 
 const CarItem = (props) => {
+
+    const { name, tagline, taglineCTA, image } = props;
+
     return (
         <View style={styles.carContainer}>
 
-            <ImageBackground source={require('../../assets/images/ModelX.jpeg')} style={styles.image}/>
+            <ImageBackground source={image} style={styles.image}/>
 
             <View style={styles.titles}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,000</Text>
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.subtitle}>
+                {tagline}
+                <Text style={styles.subtitleCTA}>
+                    {taglineCTA}
+                </Text>
+            </Text>
             </View>
 
+
+        <View style={styles.buttonsContainer}>
             <StyledButton 
             type="primary"
             content={"Custom Order"}
@@ -30,6 +40,7 @@ const CarItem = (props) => {
                 console.warn("Existing Inventory was pressed");
             }}
             />
+        </View>
 
         </View>
     );
