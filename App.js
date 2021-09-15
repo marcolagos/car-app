4eb8b4d0d9186452b7dacaefafe170bd7b18a89f
marcolagos <84419908+marcolagos@users.noreply.github.com>
2021-09-14 20:18:36 -0500

    Cover page and Assets located

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CarItem/>
+      <StatusBar style="auto"/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
